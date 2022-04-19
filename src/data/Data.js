bb24c0eea9637171b42a2f549cde73c7559a62d3
kshieldsDTS <escanor05@gmail.com>
2022-04-19 14:08:58 -0400

    add narrative node

--- src/data/Data.js
+++ src/data/Data.js
@@ -146,6 +146,13 @@ const data = [
             `Before long, you hear a satisfying click. You have unlocked the door.`
         ],
         next: 10,
+    },
+    {
+        id: 10,
+        text: [
+            `You peek out from behind the door and see a hallway to either side. The hallway is lined with doors. To the left, the hallway curves to the right. To the right, the hallway curves to the left.`
+        ],
+        next: 11,
     }
 ]
 
